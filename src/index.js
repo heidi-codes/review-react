326457358589b0bdb85924c6bd890645a501448f
Heidi Codes <109416973+heidi-codes@users.noreply.github.com>
2022-08-24 21:24:05 +0800

    feat: ChatApp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
 import Hello from "./Hello";
+import ChatApp from "./01/ChatApp";
+
+import "antd/dist/antd.css";
+
 import "./index.css";
 
 const styles = {
@@ -8,11 +12,44 @@ const styles = {
   paddingLeft: "250px"
 };
 
+const routeMap = {
+  chat: ChatApp
+};
+
 class App extends React.PureComponent {
+  handleLinkClick = (key) => {
+    // window.location.hash = `#${key}`;
+    window.history.pushState(null, "", `/#/${key}`);
+    this.forceUpdate();
+  };
   render() {
+    const currentPage = document.location.hash.replace(/#\/?/, "");
+
+    let CurrentPage = routeMap[currentPage] || Hello;
+    if (currentPage.match(/\/user\/\w+|\/list-page/)) {
+      CurrentPage = ListSample;
+    }
+    if (currentPage.match(/\/wizard\/step\/\w+/)) {
+      CurrentPage = WizardSample;
+    }
     return (
       <div style={styles}>
-        <Hello />
+        <ul className="menu-list">
+          {Object.keys(routeMap).map((key) => (
+            <li
+              key={key}
+              className={key === currentPage ? "is-active" : ""}
+              style={{ listStyle: "none" }}
+            >
+              <span className="link" onClick={() => this.handleLinkClick(key)}>
+                {key}
+              </span>
+            </li>
+          ))}
+        </ul>
+        <div style={{ padding: "30px 0" }}>
+          <CurrentPage />
+        </div>
       </div>
     );
   }
